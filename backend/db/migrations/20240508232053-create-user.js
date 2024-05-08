176318be5c96a5887e81645ede3848e739469a69
DeAndre <deandre.vidale@gmail.com>
2024-05-08 16:52:46 -0700

    updated user model and migration

---
+++ backend/db/migrations/20240508232053-create-user.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(30)
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(256)
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ backend/db/models/user.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      validate:{
+        len:[4,30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.");
+      }}
+     } },
+    email: {
+      type: DataTypes.STRING(256), 
+      allowNull: false,
+      validate:{
+        len:[3, 256],
+        isEmail: true
+      }
+    },
+    hashedPassword: {
+      allowNull: false,
+      len:[60, 60],
+      type: DataTypes.STRING
+    }
+  },{
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
