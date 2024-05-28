353f792e9de2008c3bc015e6905053780018a3c2
DeAndre <deandre.vidale@gmail.com>
2024-05-27 19:27:46 -0700

    changed ownerId model reference from Owners to Users in Spot migration Line 22

--- backend/db/migrations/20240517183346-create-spot.js
+++ backend/db/migrations/20240517183346-create-spot.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references:{
-          model:'Owners'
+          model:'Users'
         }
       },
       address: {
