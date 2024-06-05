3371b149705001a8707a6a36e00ed176f3545ced
DeAndre <deandre.vidale@gmail.com>
2024-06-04 23:58:23 -0700

    findAll, findOne spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,33 +1,216 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const { Op } = require('sequelize');
-const {Spot} = require('/Users/admin/Developer/aa-backend-portfolio-proj/backend/db/models')
+const { Op } = require("sequelize");
+const { Sequelize } = require("sequelize");
+const { Spot, SpotImage, Review, User } = require("../../db/models");
+
+const { requireAuth } = require('../../utils/auth');
+
 
 // Get all spots
 
-router.get('/', async (req, res)=>{
+const pageify = (req, res, next) => {
+  let { page, size } = req.query;
 
-    const params = {
-        where:{}
+  if (!page) page = 1;
+  if (!size) size = 20;
+
+  if (+page < 1) {
+    page = 1;
+  }
+  if (+page > 10) {
+    page = 10;
+  }
+  if (+size < 1) {
+    size = 1;
+  }
+  if (+size > 20) {
+    size = 20;
+  }
+
+  if (+page > 0 && +page <= 10 && +size > 0 && +size <= 20) {
+    req.limit = size;
+    req.offset = size * (page - 1);
+  }
+
+  //  console.log("page", page, "size", size);
+  //  console.log("limit and offset before route ---", req.limit, req.offset);
+  next();
+};
+
+router.get("/", pageify, async (req, res) => {
+  let where = {};
+ 
+  //you get query params from request
+  if (req.query) {
+    const query = req.query;
+
+    // if request includes spot name query
+
+    if (query.name) {
+      //assign name query to search params object
+      where.name = { [Op.like]: `%${query.name}%` };
     }
 
-    //you get query params from request
-    if(req.query){
-        const query = req.query;
+    // request includes page and size
+    //if it has a page spec, it doesn't have to have a size, but it probably will
+    // if it has a size, it doesn't have to have a page, but it probably will
+  }
 
-//if request includes spot name query
+  // if(req.limit){
+  //     pagination.limit = req.limit;
+  // }
+  // if(req.offset){
+  //     pagination.offset = req.offset;
+  // }
 
-        if(query.name){
-//assign name query to search params object
-            params.where.name = { [Op.like]: `%${query.name}%` }
-        }
+  let spots = await Spot.findAll({
+    where,
+    include: [
+      {
+        model: SpotImage,
+        attributes: [],
+      },
+      {
+        model: Review,
+        attributes: [],
+      },
+    ],
+    attributes: [
+        "id",
+        "ownerId",
+        "address",
+        "city",
+        "state",
+        "country",
+        "lat",
+        "lng",
+        "name",
+        "description",
+        "price",
+        "createdAt",
+        "updatedAt",
 
-    }
+        // for some reason these lines limits the query to 1 result
+        // [Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 'avgRating'],
 
-    let spots = await Spot.findAll(params);
+        // for some reason these lines make the query lose connection to the SpotImages table if any code is added after them
+        [Sequelize.col("SpotImages.url"), "previewImage"]
+      ]
+  });
 
-    return res.json(spots)
+  
+
+  res.json(spots);
+});
+
+
+
+//Get all spots owned by current user
+ //requires auth: only current user should be able to access this route, otherwise throw authorization error
+
+ router.get("/current", requireAuth, async(req,res)=>{
+  
+  const spots = await Spot.findAll({
+    where:{
+      ownerId: req.user.id
+    },
+    include: [
+      {
+        model: SpotImage,
+        attributes: [],
+      },
+      {
+        model: Review,
+        attributes: [],
+      },
+    ],
+    attributes: [
+        "id",
+        "ownerId",
+        "address",
+        "city",
+        "state",
+        "country",
+        "lat",
+        "lng",
+        "name",
+        "description",
+        "price",
+        "createdAt",
+        "updatedAt",
+
+        // for some reason these lines limits the query to 1 result
+        // [Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 'avgRating'],
+
+        // for some reason these lines make the query lose connection to the SpotImages table if any code is added after them
+        [Sequelize.col("SpotImages.url"), "previewImage"]
+      ]
+  })
+
+  res.json(spots);
+
+});
+
+// get spot details by id
+router.get("/:spotId", async (req, res)=>{
+
+const spot = await Spot.findOne({
+  where:{
+    id: req.params.spotId
+  },
+  include: [
+    {
+      model: SpotImage,
+      attributes: {
+        exclude:['spotId','createdAt', 'updatedAt']
+      },
+    },
+    {
+      model: Review,
+      attributes: [],
+    },
+    {
+      model: User,
+      as: 'owner',
+      attributes: ['id','firstName','lastName'],
+    },
+  ],
+  attributes: [
+      "id",
+      "ownerId",
+      "address",
+      "city",
+      "state",
+      "country",
+      "lat",
+      "lng",
+      "name",
+      "description",
+      "price",
+      "createdAt",
+      "updatedAt",
+      [Sequelize.fn('COUNT', Sequelize.col('Reviews.id')), 'numReviews'],
+      [Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 'avgStarRating']
+    ]
+
+
+});
+
+if(spot.id === null){
+res.status(404);
+res.json({
+  "message": "Spot couldn't be found"
+});
+}
+
+  res.json(spot);
+
+
+
+
 
 })
+
 
 module.exports = router;
