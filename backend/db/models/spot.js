3e27f9e75cb25c17455e18aca56abb2a764cb418
DeAndre <deandre.vidale@gmail.com>
2024-06-05 22:56:52 -0700

    updated spot and spotimage constraints

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -21,7 +21,8 @@ module.exports = (sequelize, DataTypes) => {
         onDelete: 'CASCADE'
       });
       Spot.hasMany(models.SpotImage,{
-        foreignKey: 'spotId'
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE'
       });
       Spot.hasMany(models.Review,{
         foreignKey: 'spotId'
