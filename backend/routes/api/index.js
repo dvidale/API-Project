7f9f7c26d62d2a8896b70f0f80eb0035ed8f425e
DeAndre <deandre.vidale@gmail.com>
2024-06-10 11:15:55 -0700

    created delete a spot image route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,8 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
+const spotImagesRouter = require('./spot-images.js');
+
 
 const { restoreUser } = require('../../utils/auth.js');
 
@@ -14,6 +16,7 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 router.use('/spots', spotsRouter);
+router.use('/spot-images', spotImagesRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
 
