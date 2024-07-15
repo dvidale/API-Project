0a39bb7ae8d9baeeceb0e34ee354c0ca5654080d
DeAndre <deandre.vidale@gmail.com>
2024-07-15 09:50:26 -0700

    added dropdown to Navigation

--- frontend/components/Navigation/Navigation.jsx
+++ frontend/components/Navigation/Navigation.jsx
@@ -1,47 +1,24 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import { useSelector } from "react-redux";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
 
 import "./Navigation.css";
 
 const Navigation = ({ isLoaded }) => {
   const sessionUser = useSelector((state) => state.session.user);
 
-  const sessionLinks = sessionUser ? (
-    <ProfileButton user={sessionUser} />
-  ) : (
-    <>
-      <OpenModalButton
-        buttonText="Log In"
-        modalComponent={<LoginFormModal />}
-      />
-      <OpenModalButton
-        buttonText="Sign Up"
-        modalComponent={<SignupFormModal />}
-      />
-    </>
-  );
-
   return (
-    <header>
-    <nav>
-      <div id="nav-left">
-      <NavLink to="/">
-        {<img alt="Home" src="./heirbnb_site_logo_v1_full_size.png" />}
-      </NavLink>
-      </div>
-     
-<div id="nav-right">
-{isLoaded && sessionLinks}
+      <ul>
+<li> 
+      <NavLink to="/"> {<img alt="Home" src="./heirbnb_site_logo_v1_full_size.png" />} </NavLink>
+      </li>
 
-</div>
-      
-    </nav>
-    </header>
-  );
-};
-
+{isLoaded && (
+<li>
+<ProfileButton user={sessionUser} /> 
+   </li>
+  )}
+</ul>
+);
+}
 export default Navigation;
